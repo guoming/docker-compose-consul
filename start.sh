5ded6f5edf42b1b81a3cbc3db104d5ef275c3330
guoming <geniusming@qq.com>
2019-03-04 18:01:46 +0800

    first

---
+++ start.sh
@@ -0,0 +1,8 @@
+﻿mkdir -p /mnt/consul/node1
+mkdir -p /mnt/consul/node2
+mkdir -p /mnt/consul/node3
+
+chmod 777 /mnt/consul/node1
+chmod 777 /mnt/consul/node2
+chmod 777 /mnt/consul/node3
+docker stack deploy --compose-file docker-compose.yml consul
