322546b80f831df4fba8bf8a916b27897bc8373d
guoming <geniusming@qq.com>
2022-02-17 11:06:25 +0800

    init

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
-﻿mkdir -p /mnt/consul
-chmod 777 /mnt/consul
-
-
-docker node update --label-add consul=node1 sz-new-test
-docker node update --label-add consul=node2 sz-itdev4
-docker stack deploy --compose-file docker-compose.yml consul
+﻿docker-compose up -d
+docker exec -it consul-dc2-node1 consul join -wan consul-dc1-node1
